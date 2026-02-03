796047e82166875d963d74b2ca50bc2fd299fcb5
cgnomc1 <cgnomc@gmail.com>
2026-02-03 20:13:08 +0100

    ready to deploy?

--- apps/container-shell/src/config/mfe-urls.ts
+++ apps/container-shell/src/config/mfe-urls.ts
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export const MFE_URLS = {
-  client: isProd ? 'https://pwa-easy-rental-mfe-client.vercel.app' : 'http://localhost:3001/client',
-  agency: isProd ? 'https://pwa-easy-rental-agency.vercel.app' : 'http://localhost:3002/agency',
-  organisation: isProd ? 'https://pwa-easy-rental-org.vercel.app' : 'http://localhost:3003/organisation',
+  client: isProd ? 'https://pwa-easy-rental-mfe-client.vercel.app/client' : 'http://localhost:3001/client',
+  agency: isProd ? 'https://pwa-easy-rental-agency.vercel.app/agency' : 'http://localhost:3002/agency',
+  organisation: isProd ? 'https://pwa-easy-rental-org.vercel.app/organisation' : 'http://localhost:3003/organisation',
 };
