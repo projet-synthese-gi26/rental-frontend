0cc79accaaecd50f9d3d9d30887b604abf28dc24
cgnomc1 <cgnomc@gmail.com>
2026-03-16 12:10:23 +0100

    thanks jordan

--- packages/shared-services/src/api/rental.service.ts
+++ packages/shared-services/src/api/rental.service.ts
@@ -27,4 +27,7 @@ export const rentalService = {
   getClientRentalsHistory: () => client.get<any[]>('/api/rentals/client/rentals/history'),
 
   getByAgency: (agencyId: string) => client.get<any[]>(`/api/rentals/agency/${agencyId}/rentals`),
+
+  getRentalDetails: (id: string) => client.get<any[]>(`/api/rentals/${id}/details`),
+
 };
